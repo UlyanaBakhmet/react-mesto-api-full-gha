484fbcef652565b01a2f90e8065a5ea80d3bb05b
Ulyana Bakhmet <buyarova.u@yandex.ru>
2023-07-17 12:29:42 +0300

    15.change apiConfig

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,8 +1,9 @@
 //экземпляр класса АПИ для работы с сервером
 export const apiConfig = {
-    url: 'https://mesto.nomoreparties.co/v1/cohort-54',
+    // url: 'https://mesto.nomoreparties.co/v1/cohort-54',
+    url: 'https://api.bakhmet.nomoredomains.xyz/',
     headers: {
-    authorization: '070a38fd-7788-4fe4-9dbb-7187d1c3352a',
-    "Content-Type": "application/json",
+    // authorization: '070a38fd-7788-4fe4-9dbb-7187d1c3352a',
+    'Content-Type': 'application/json',
   },
   };
